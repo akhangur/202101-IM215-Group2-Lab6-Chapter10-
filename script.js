e8787b70171657ee250deafefaddb55d77f7e1a0
BONY BABU <65872018+bbabu1@users.noreply.github.com>
2021-04-12 16:15:27 -0400

    Js

--- script.js
+++ script.js
@@ -8,4 +8,23 @@ $(document).ready(function() {
             $(select).append('<option value"' + result[i].id +'">' + result[i].name +'</option>')   
         }
     }); 
+    $.get("http://www.randyconnolly.com/funwebdev/services/visits/os.php")
+    .done(function(result) {
+        console.log(result);
+        select = $("#filterOS")
+        for(var i=0;i<result.length;i++)
+        {
+            $(select).append('<option value"' + result[i].id +'">' + result[i].name +'</option>')   
+        }
+    }); 
+    $.get("http://www.randyconnolly.com/funwebdev/services/visits/countries.php?continent=EU")
+    .done(function(result) {
+        console.log(result);
+        select = $("#filterCountry")
+        for(var i=0;i<result.length;i++)
+        {
+            $(select).append('<option value"' + result[i].id +'">' + result[i].name +'</option>')   
+        }
+    }); 
+    
 })
