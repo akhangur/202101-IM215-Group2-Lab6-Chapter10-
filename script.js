287bf7427292ef6f914e6d7be3f26aa78359e604
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-04-12 20:43:38 -0400

    Step 5

--- script.js
+++ script.js
@@ -37,6 +37,60 @@ $(document).ready(function() {
             $(tbody).append('<tr><td>'+result[i].id+'</td><td>' +date.toDateString() +'</td><td>'+result[i].country +'</td><td>'+result[i].browser +'</td><td>'+result[i].operatingSystem +'</td></tr>')   
         }
     }); 
-    }); 
-    
+
+    $("#filterCountry").change(function()
+    {
+        displayFunction();
+        
+    })
+    $("#filterBrowser").change(function()
+    {
+        displayFunction();
+    })
+   
+    $("#filterOS").change(function()
+    {
+        displayFunction();
+    })
+    function displayFunction()
+    {
+        tbody = $("#visitsBody")
+        tbody.empty();
+        browser=$("#filterBrowser").val();
+        country=$("#filterCountry").val();
+        os=$("#filterOS").val();
+        console.log("browser : "+browser+" country : "+country+" os : "+os)
+        console.log(country);
+        var result=$.grep(data, function (item,i) {
+            if(browser==0&&os==0&&country==0)
+            return (data);
+
+            else if(browser==0&&os==0)
+            return(item.country==country)
+
+            else if(os==0&&country==0)
+            return(item.browser==browser)
+
+            else if(browser==0&&country==0)
+            return(item.operatingSystem==os)
+
+            else if(browser==0)
+            return(item.country==country&&item.operatingSystem==os)
+
+            else if(country==0)
+            return(item.browser==browser&&item.operatingSystem==os)
+
+            else if(os==0)
+            return(item.country==country&&item.browser==browser)
+
+            else
+            return(item.country==country&&item.operatingSystem==os&&item.browser==browser)
+        });
+        console.log(result);
+        for(var i=0;i<result.length;i++)
+        {
+            var date = new Date( result[i].visit_date)
+            $(tbody).append('<tr><td>'+result[i].id+'</td><td>' +date.toDateString() +'</td><td>'+result[i].country +'</td><td>'+result[i].browser +'</td><td>'+result[i].operatingSystem +'</td></tr>')   
+        }
+    }
 })
